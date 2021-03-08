71b61035fe709768a81f403b39213afed1965dec
zhengis-alinur <zhengis.alinur.erzhan@gmail.com>
2021-03-08 23:29:04 +0600

    init: find solution for task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(typeof array == 'undefined') {
+    return 0;
+  } else if(array.length != 0) {
+    let min = array[0];
+    for(i = 1; i < array.length; i++) {
+      if(array[i] < min) {
+        min = array[i]
+      }
+    }
+    return min;
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof array == 'undefined') {
+    return 0;
+  } else if(array.length != 0){
+    let max = array[0];
+    for(i = 1; i < array.length; i++) {
+      if(array[i] > max) {
+        max = array[i]
+      }
+    }
+    return max;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof array == 'undefined') {
+    return 0;
+  } else if(array.length != 0){
+    let sum = array.reduce((sum, cur) => sum + cur);
+    return sum/array.length;
+  } else {
+    return 0;
+  }
 }
